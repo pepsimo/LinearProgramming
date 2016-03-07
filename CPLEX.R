7d75fb6f6d925a438f7141ae6149d6c2e7399715
Pep Simo <escacoix@gmail.com>
2016-03-07 10:22:32 +0100

    Update CPLEX.R

--- CPLEX.R
+++ CPLEX.R
@@ -10,13 +10,13 @@ CPLEX <- function(model, method="CPLEX_LP", decimal=0){
   
   x.sol <- Rglpk_solve_LP(x$objective, x$constraints[[1]], x$constraints[[2]], x$constraints[[3]], x$bounds, x$types, x$maximum)
   
-  x.sol.df <- as.data.frame(x.sol$solution)
+  if (x.sol$status > 0) {return ("Status: Optimal solution has not been found")}
   
-  x.sol.df <- rbind(x.sol.df, c(x.sol$optimum))
+  x.sol.df <- as.data.frame(x.sol$solution)
+  x.sol.df <- rbind(x.sol.df, (x.sol$optimum))
   
   rownames(x.sol.df) <- c(attr(x, "objective_vars_names"),"obj")
   colnames(x.sol.df) <- "Solution"
  
   return(x.sol.df)
 }
-
